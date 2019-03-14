8e141a6cb6d0ee747d907f480f3029d595b77066
byokke <vas.byokke@gmail.com>
2019-03-14 20:48:03 +0900

    fixed package.json dependecies

--- web/hocs/with-styles.js
+++ web/hocs/with-styles.js
@@ -1,11 +1,8 @@
 import React from "react"
 import Head from "next/head"
-import NProgress from "next-nprogress/component"
 import ruRU from "antd/lib/locale-provider/ru_RU"
 import { LocaleProvider } from "antd"
 import { createGlobalStyle } from "styled-components"
-
-import { Colors } from "../config/index"
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -18,7 +15,6 @@ const GlobalStyles = createGlobalStyle`
 const WithGlobalStyles = Component => props => (
   <LocaleProvider locale={ruRU}>
     <React.Fragment>
-      <NProgress color={Colors.primary} />
       <Component {...props} />
       <GlobalStyles />
     </React.Fragment>
